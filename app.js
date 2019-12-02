9179c93472f455671f2225a862893b6f36251543
boshan200 <boshan200@gmail.com>
2019-12-02 15:48:59 +0800

    mongodb setup

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes       = require("./routes/index");
 
 //mongoose set + ejs template
-mongoose.connect("mongodb+srv://boshan:Kassadin100@cluster0-avpci.mongodb.net/test?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://boshan:boshan200@cluster0-avpci.mongodb.net/test?retryWrites=true&w=majority", {
     useNewUrlParser: true, 
     useUnifiedTopology: true
 }).then(() => {
